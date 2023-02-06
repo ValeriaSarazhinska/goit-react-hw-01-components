7e45883bc4edcfdc484c113f03356e000ba6aeef
ValeriaSarazhinska <lera.sarazhinska@gmail.com>
2023-02-06 16:26:52 +0200

    refactored hw-01

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -24,7 +24,12 @@ const Statistics = ({title, stats}) => {
 }
 Statistics.propTypes = {
   title: PropTypes.string.isRequired,
-  stats: PropTypes.array,
+  stats: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    percentage: PropTypes.number.isRequired,
+  }).isRequired
+
 }
 
 export default Statistics
--- src/components/TransactionHistory.jsx
+++ src/components/TransactionHistory.jsx
@@ -25,7 +25,13 @@ const TransactionHistory = ({items}) => {
   )
 }
 TransactionHistory.propTypes = {
-  items: PropTypes.array
+  items: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    amount: PropTypes.string.isRequired,
+    currency: PropTypes.string.isRequired,
+  }).isRequired)
+    .isRequired
 }
 
 export default TransactionHistory
